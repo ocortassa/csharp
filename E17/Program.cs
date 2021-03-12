731dc3bdfd441ffdc28039ca0a1c8d57e5a10874
Omar Cortassa <omar.cortassa@gmail.com>
2021-03-12 22:25:07 +0100

    complete import

---
+++ E17/Program.cs
@@ -0,0 +1,457 @@
+﻿/*=================================================
+* @Autore: DEMICHELIS ANDREA
+* @Numerazione: ESERCIZIO 17
+* @Titolo: Ricerche Sequenziali :
+*            - Caso1 pag.91 (Vettore Disgiunto e non ordinato);
+*            - Caso2 pag.92 (Vettore con Ripetizioni e non ordinato);
+*            - Caso3 pag.93 (Vettore Disgiunto e Ordinato);
+*            - Caso4 pag.94 (Vettore con Ripetizioni e Ordinato);
+*
+* @Classe: 3A INF
+* @Data: 19/01/2021 
+*===================================================*/
+
+using System;
+/*
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+*/
+
+namespace Esercizio17
+{
+    class Program
+    {
+
+        static Random rnd = new Random();
+        const int Max = 100;
+
+        static void Main(string[] args)
+        {
+
+            int[] vNum = new int[1];
+            int lunVet = 0;
+            string scelta = string.Empty;
+
+            bool car1OK = false, car2OK = false, car3OK = false, car4OK = false;
+
+            do
+            {
+
+                // Visualizza le singole Voci del Menù
+                disegnaMenu();
+
+                // Acquisisco la Scelta dell' Utente
+                scelta = Console.ReadLine();
+
+                switch (scelta)
+                {
+                    case "1":
+                        // Chiede in Input la Lunghezza del Vettore
+                        lunVet = gesInputLunVet();
+                        // Definisco la lunghezza del Vettore
+                        vNum = new int[lunVet];
+                        car1OK = false;
+                        car2OK = false;
+                        car3OK = false;
+                        car4OK = false;
+                        break;
+
+                    case "2":
+                        // Carico Vettore Disgiunto e NON ordinato [car1OK]
+                        if (lunVet != 0)
+                        {
+                            caricaVetDisgiunto(vNum, lunVet, Max + 50);
+                            car1OK = true;
+                            Console.WriteLine("");
+                            Console.WriteLine("Caricamento Vettore Disgiunto e NON ordinato effettuato con successo.");
+                        }
+                        else
+                            Console.WriteLine("Lunghezza del Vettore non inserita.");
+                        break;
+
+                    case "3":
+                        // Carico Vettore con Ripetizioni e NON ordinato [car2OK]
+                        if (lunVet != 0)
+                        {
+                            caricaVetRipetizioni(vNum, lunVet);
+                            car2OK = true;
+                            Console.WriteLine("");
+                            Console.WriteLine("Caricamento Vettore con Ripetizioni e NON ordinato effettuato con successo.");
+                        }
+                        else
+                            Console.WriteLine("Lunghezza del Vettore non inserita.");
+                        break;
+
+                    case "4":
+                        // Carico Vettore Disgiunto e Ordinato [car3OK]
+                        if (lunVet != 0)
+                        {
+                            caricaVetDisgiuntoOrd(vNum, lunVet);
+                            car3OK = true;
+                            Console.WriteLine("");
+                            Console.WriteLine("Caricamento Vettore Disgiunto e Ordinato effettuato con successo.");
+                        }
+                        else
+                            Console.WriteLine("Lunghezza del Vettore non inserita.");
+                        break;
+
+                    case "5":
+                        // Carico Vettore con Ripetizioni e Ordinato [car4OK]
+                        if (lunVet != 0)
+                        {
+                            caricaVetRipetizioniOrd(vNum, lunVet);
+                            car4OK = true;
+                            Console.WriteLine("");
+                            Console.WriteLine("Caricamento Vettore con Ripetizioni e Ordinato effettuato con successo.");
+                        }
+                        else
+                            Console.WriteLine("Lunghezza del Vettore non inserita.");
+                        break;
+
+                    case "6":
+                        //Visualizza Vettore
+                        if (lunVet != 0)
+                        {
+                            if (car1OK || car2OK || car3OK || car4OK)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Vettore:");
+                                Console.WriteLine("");
+                                visualizzaVettore(vNum, lunVet);
+                            }
+                            else
+                                Console.WriteLine("Vettore non caricato.");
+                        }
+                        else
+                            Console.WriteLine("Lunghezza del Vettore non inserita.");
+                        break;
+
+                    case "7":
+                        // Ricerca su Vettore Disgiunto e NON ordinato
+                        if (lunVet != 0)
+                        {
+                            if (car1OK)
+                            {
+                                ricercaVetDisgiunto(vNum, lunVet);
+                            }
+                            else
+                                Console.WriteLine("Vettore non caricato.");
+                        }
+                        else
+                            Console.WriteLine("Lunghezza del Vettore non inserita.");
+                        break;
+
+
+                    case "8":
+                        // Ricerca su Vettore con Ripetizioni e NON ordinato
+                        if (lunVet != 0)
+                        {
+                            if (car2OK)
+                            {
+                                ricercaVetRipetizioni(vNum, lunVet);
+                            }
+                            else
+                                Console.WriteLine("Vettore non caricato.");
+                        }
+                        else
+                            Console.WriteLine("Lunghezza del Vettore non inserita.");
+                        break;
+
+                    case "9":
+                        break;
+
+                    case "10":
+                        break;
+
+                    case "0":
+                        break;
+
+                    default:
+                        Console.WriteLine("");
+                        Console.WriteLine("Scelta non valida.");
+                        break;
+                }
+
+                // Attesa Utente
+                if (scelta != "0")
+                    attesa();
+
+            }
+            while (scelta != "0");
+
+        }
+
+        /****************/
+        /* Disegna Menù */
+        /****************/
+        static void disegnaMenu()
+        {
+            //Console.Clear();
+            Console.WriteLine("Ricerche Sequenziali");
+
+            Console.WriteLine("");
+            Console.WriteLine("1 - Inserimento Lunghezza Vettore");
+            Console.WriteLine("2 - Carico Vettore Disgiunto e NON ordinato");
+            Console.WriteLine("3 - Carico Vettore con Ripetizioni e NON ordinato");
+            Console.WriteLine("4 - Carico Vettore Disgiunto e Ordinato");
+            Console.WriteLine("5 - Carico Vettore con Ripetizioni e Ordinato");
+            Console.WriteLine("6 - Visualizza Vettore");
+            Console.WriteLine("");
+            Console.WriteLine("7 - CASO 1 - Ricerca su Vettore Disgiunto e NON ordinato");
+            Console.WriteLine("8 - CASO 2 - Ricerca su Vettore con Ripetizioni e NON ordinato");
+            Console.WriteLine("9 - CASO 3 - Ricerca su Vettore Disgiunto e Ordinato");
+            Console.WriteLine("10 - CASO 4 - Ricerca su Vettore con Ripetizioni e Ordinato");
+            Console.WriteLine("");
+            Console.WriteLine("0 - Uscire");
+            Console.WriteLine("");
+            Console.Write("Scelta ==> ");
+        }
+
+        /******************/
+        /* Attesa Utentre */
+        /******************/
+        static void attesa()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Premi un tasto per continuare ...");
+            Console.ReadKey();
+        }
+
+        /***************************/
+        /* Input Lunghezza Vettore */
+        /***************************/
+        static int gesInputLunVet()
+        {
+            int lunghezza = 0;
+            bool errore;
+
+            do
+            {
+                errore = false;
+
+                Console.Write("Inserisci la Lunghezza del Vettore (1.." + Max + ") ==> ");
+                try
+                {
+                    lunghezza = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Lunghezza del Vettore non valida. Errore (" + ex.Message + ")");
+                    errore = true;
+                }
+
+                if (!errore)
+                {
+                    if (lunghezza <= 0 || lunghezza > Max)
+                    {
+                        Console.WriteLine("Lunghezza del Vettore non valida.");
+                        errore = true;
+                    }
+                }
+            }
+            while (errore);
+
+            // Restuisco la Lunghezza inserita
+            return lunghezza;
+        }
+
+        /***************************/
+        /* Input Numero da Ricerca */
+        /***************************/
+        static int gesInputNumero()
+        {
+            int Numero = 0;
+            bool errore;
+
+            do
+            {
+                errore = false;
+
+                Console.Write("Inserisci il Numero da Ricercare (1.." + (Max + 50) + ") ==> ");
+                try
+                {
+                    Numero = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Numero da Ricercare non valido. Errore (" + ex.Message + ")");
+                    errore = true;
+                }
+
+                if (!errore)
+                {
+                    if (Numero <= 0 || Numero > (Max + 50))
+                    {
+                        Console.WriteLine("Numero da Ricercare non valido.");
+                        errore = true;
+                    }
+                }
+            }
+            while (errore);
+
+            return Numero;
+        }
+
+        /****************************************************/
+        /* Caricamento del Vettore Disgiunto e NON ordinato */
+        /****************************************************/
+        static void caricaVetDisgiunto(int[] V, int lun, int NumMax)
+        {
+            int I, num;
+
+            for (I = 0; I < lun; I++)
+            {
+                do
+                {
+                    num = rnd.Next(1, NumMax);
+                }
+                while (esiste(V, I, num));
+                V[I] = num;
+            }
+        }
+
+        /*******************************************/
+        /* Controlla duplicazione Numero  generato */
+        /*******************************************/
+        static bool esiste(int[] V, int lun, int x)
+        {
+            bool trovato = false;
+            int I = 0;
+
+            while ((I < lun) && !(trovato))
+            {
+                if (V[I] == x)
+                    trovato = true;
+                I++;
+            }
+
+            // Versione per chi non vuole usare il WHILE
+            /*
+            for (I = 0; I < lun; I++)
+            {
+                if (V[I] == x)
+                    trovato = true;
+            }
+            */
+
+            return trovato;
+        }
+
+        /*************************************************/
+        /* Carico Vettore con Ripetizioni e NON ordinato */
+        /*************************************************/
+        static void caricaVetRipetizioni(int[] V, int lun)
+        {
+            int I;
+
+            for (I = 0; I < lun; I++)
+                V[I] = rnd.Next(1, (Max + 50));
+        }
+
+        /***************************************/
+        /* Carico Vettore Disgiunto e Ordinato */
+        /***************************************/
+        static void caricaVetDisgiuntoOrd(int[] V, int lun)
+        {
+            int I, valPrec = 1;
+
+            for (I = 0; I < lun; I++)
+            {
+                V[I] = valPrec + rnd.Next(1, 3);
+                valPrec = V[I];
+            }
+        }
+
+        /*********************************************/
+        /* Carico Vettore con Ripetizioni e Ordinato */
+        /*********************************************/
+        static void caricaVetRipetizioniOrd(int[] V, int lun)
+        {
+            int I, valPrec = 1;
+
+            for (I = 0; I < lun; I++)
+            {
+                V[I] = valPrec + rnd.Next(0, 3);
+                valPrec = V[I];
+            }
+        }
+
+        /******************************/
+        /* Visulizzazione del Vettore */
+        /******************************/
+        static void visualizzaVettore(int[] V, int lun)
+        {
+            int I;
+
+            for (I = 0; I < lun; I++)
+                Console.Write(V[I] + " | ");
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        /********************************************************/
+        /* CASO 1 - Ricerca su Vettore Disgiunto e NON ordinato */
+        /********************************************************/
+        static void ricercaVetDisgiunto(int[] V, int lun)
+        {
+            int x;
+            int I = 0;
+
+            Console.Clear();
+            x = gesInputNumero();
+
+            while (!((V[I] == x) || (I == lun - 1)))
+            {
+                I++;
+            }
+
+            Console.WriteLine("");
+
+            if (V[I] == x)
+                Console.WriteLine(
+                    "Numero " + x +
+                    " trovato in posizione " + (I + 1) + ".");
+            else
+                Console.WriteLine("Numero " + x + " non trovato.");
+
+            Console.WriteLine("");
+
+        }
+
+        /**************************************************************/
+        /* CASO 2 - Ricerca su Vettore con Ripetizioni e NON ordinato */
+        /**************************************************************/
+        static void ricercaVetRipetizioni(int[] V, int lun)
+        {
+            int x, I;
+            bool trovato = false;
+
+            Console.Clear();
+            Console.WriteLine("");
+            x = gesInputNumero();
+
+            Console.WriteLine("");
+
+            for (I = 0; I < lun; I++)
+            {
+                if (V[I] == x)
+                {
+                    Console.WriteLine(
+                    "Numero " + x +
+                    " trovato in posizione " + (I + 1) + ".");
+                    trovato = true;
+                }
+            }
+
+            if (!trovato)
+                Console.WriteLine("Numero " + x + " non trovato.");
+
+            Console.WriteLine("");
+
+        }
+
+    }
+}
---
+++ E19/Program.cs
@@ -0,0 +1,311 @@
+﻿using System;
+
+namespace Esercizio19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pedG = 0, pedR = 0;
+            char c = ' ';
+            int posI = 7, PosOLD = 7;
+            int N = 0;
+            int Turno = 1;
+            /*
+                Turno = 1 ==> ROSSO
+                Turno = 2 ==> GIALLO
+            */
+
+            int[,] matrice = new int[8, 8];
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            /*
+            InizializzaMatrice(ref matrice);
+            StampaMatrice(ref matrice);
+            System.Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+            */
+
+            // Crea Struttura
+            Struttura();
+            VisualizzaMatrice(ref matrice);
+
+            // Gestisce Punteggio
+            Punteggio(Turno, pedG, pedR);
+
+            // Gestisce la possibilità di inserire la Pedina
+            Pedina(posI, ref PosOLD, N, Turno);
+
+            do
+            {
+
+                // Controlla pressione Carattere
+                if (Console.KeyAvailable)
+                {
+
+                    c = Convert.ToChar(Console.ReadKey(false).Key);
+                    N = Convert.ToInt32(c);
+
+                    switch (N)
+                    {
+                        case 37:
+                            // Freccia SX
+                            if (posI > 7)
+                                posI -= 4;
+                            break;
+
+                        case 39:
+                            // Freccia DX
+                            if (posI < 35)
+                                posI += 4;
+                            break;
+
+                        case 40:
+                            // Freccia DOWN
+
+                            // Imposta valore matrice
+                            int pedinaPiazzata = ImpostaPedina(ref matrice, posI, Turno);
+                            if (pedinaPiazzata == 0) {
+                                continue;
+                            }
+
+                            if (Turno == 1)
+                            {
+                                pedR++;
+                                Turno++;
+                            }
+                            else
+                            {
+                                pedG++;
+                                Turno--;
+                            }
+
+                            // Gestisce Punteggio
+                            Punteggio(Turno, pedG, pedR);
+                            VisualizzaMatrice(ref matrice);
+
+                            ControllaChiVince(ref matrice);
+
+                            break;
+
+                        default:
+                            break;
+
+                    }
+                    Pedina(posI, ref PosOLD, N, Turno);
+
+                }
+
+            }
+            while (c != 81);
+
+
+        }
+
+        static void InizializzaMatrice(ref int[,] m) {
+            for (int r = 0; r < 8; r++) {
+                for (int c = 0; c < 8; c++) {
+                    m[r, c] = 0;
+                }
+            }
+        }
+
+        static void StampaMatrice(ref int[,] m) {
+            for (int r = 0; r < 8; r++) {
+                for (int c = 0; c < 8; c++) {
+                   Console.Write(m[r, c] + ",");
+                }
+                Console.WriteLine("");
+            }            
+        }
+
+        static int ImpostaPedina(ref int[, ] mat, int col, int colore) {
+            int cMatrice = (col - 7) / 4;
+            // Verifica colonna piena
+            if (mat[0, cMatrice] != 0) {
+                // Colonna piena
+                return 0;
+            }
+            // Indivia prima cella vuota
+            int rMatrice = 7;
+            for(; rMatrice >= 0; rMatrice--) {
+                if (mat[rMatrice, cMatrice] == 0) {
+                    break;
+                }
+            }
+            mat[rMatrice, cMatrice] = colore;
+            return 1;
+        }
+
+        static void VisualizzaMatrice(ref int[,] m)
+        {
+            int R, C;
+            int ColI = 5;
+            int ColY = ColI;
+
+            for (R = 5; R < 21; R+=2)
+            {
+                for (C = 7; C < 38; C+=4)
+                {
+                    Console.SetCursorPosition(C, R);
+                    int rMatrice = (R-5) / 2;
+                    int cMatrice = (C-7) / 4;
+                    if (m[rMatrice, cMatrice] == 0) {
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
+                    if (m[rMatrice, cMatrice] == 1) {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                    }
+                    if (m[rMatrice, cMatrice] == 2) {
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                    }
+                        Console.Write(" ");
+                }
+            }
+            Console.SetCursorPosition(0, 0);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+        
+        }
+
+        static void ControllaChiVince(ref int[,] mat) {
+            // Controlla verticale
+            int haVinto = ControllaVerticale(ref mat);
+            if (haVinto != 0) {
+                Console.WriteLine("Ha vinto il colore: " + haVinto + " !!!!!!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            // Controlla orizzontale
+            /*
+            int haVinto = ControllaOrizzontale(ref mat);
+            if (haVinto != 0) {
+                Console.WriteLine("Ha vinto il colore: " + haVinto + " !!!!!!");
+            }
+            int haVinto = ControllaDiagonale(ref mat);
+            if (haVinto != 0) {
+                Console.WriteLine("Ha vinto il colore: " + haVinto + " !!!!!!");
+            }
+            */
+        }
+
+        static int ControllaVerticale(ref int[,] mat) {
+            int contatore = 1;
+            for (int colonna = 0; colonna < 7; colonna++) {
+                int coloreDaControllare = mat[7,colonna];
+                for(int riga = 6; riga >= 0; riga--) {
+                    if (mat[riga, colonna] == coloreDaControllare) {
+                        contatore++;
+                    } else {
+                        coloreDaControllare = mat[riga, colonna];
+                        contatore = 0;
+                    }
+                    if (contatore == 4) {
+                        return coloreDaControllare;
+                    }
+                }
+            }
+            return 0;
+        }
+
+// ------------------------------------------------------------------------------------------------
+
+
+        static void Struttura()
+        {
+            int R, C;
+            int ColI = 5;
+            int ColY = ColI;
+
+            Console.Clear();
+
+            for (C = ColI; C < 65; C++)
+            {
+                Console.SetCursorPosition(C, 2);
+                Console.Write("-");
+            }
+
+            for (R = 4; R < 21; R++)
+            {
+                for (C = ColI; C < 38; C++)
+                {
+
+                    Console.SetCursorPosition(C, R);
+                    if (C == ColY)
+                    {
+                        Console.Write("|");
+                        ColY = ColY + 4;
+                    }
+                    else
+                    {
+                        if (R % 2 == 0)
+                            Console.Write("=");
+                        else
+                            Console.Write(" ");
+                    }
+
+                }
+                ColY = ColI;
+            }
+            Console.SetCursorPosition(0, 0);
+
+        }
+
+        static void Punteggio(int p, int PG, int PR)
+        {
+            Console.SetCursorPosition(45, 4);
+            Console.Write("Turno");
+
+            Console.SetCursorPosition(52, 4);
+            Console.ForegroundColor = ConsoleColor.Black;
+            if (p == 1)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.Write("  ROSSO  ");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.Write(" GIALLO  ");
+            }
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.SetCursorPosition(45, 6);
+            Console.Write("Pedine ROSSO: " + PR);
+            Console.SetCursorPosition(45, 7);
+            Console.Write("Pedine GIALLO: " + PG);
+        }
+
+        static void Pedina(int newPOS, ref int oldPOS, int nChar, int p)
+        {
+            // Pulisce la vecchia posizione
+            Console.SetCursorPosition(oldPOS, 1);
+            Console.Write("     ");
+
+            Console.SetCursorPosition(newPOS, 1);
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            if (p == 1)
+                Console.BackgroundColor = ConsoleColor.Red;                
+            else
+                Console.BackgroundColor = ConsoleColor.Yellow;
+            
+            Console.Write("#");
+
+            // Mi sposto o verso DX o verso SX
+            if (nChar == 37 && oldPOS > 7)
+                oldPOS -= 4;
+            if (nChar == 39 && oldPOS < 35)
+                oldPOS += 4;
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+        }
+
+    }
+}
