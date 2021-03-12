671f7d9f352f6250f9ea1873465b10a9d47e55be
Omar Cortassa <omar.cortassa@gmail.com>
2021-03-12 22:12:02 +0100

    first import

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 10;
+            Incrementa(a);
+            Console.WriteLine("a: " + a);
+            //a = IncrementaERitorna(a);
+            Console.WriteLine("a: " + IncrementaERitorna(a));
+        }
+
+        private static void Incrementa(int v) 
+        {
+            int x = 1;
+            v = v + x;
+            Console.WriteLine("v: " + v);            
+        }
+
+        private static int IncrementaERitorna(int v) {
+            int x = 1;
+            v = v + x;
+            Console.WriteLine("v:" + v);
+            return v;
+        }
+        
+    }
+}
